ea8c6053781d157befb590dee387b7569070567e
Dana <danayim@bu.edu>
2024-10-07 13:09:13 -0400

    fixed url

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/mini_fb/', views.random_quote, name='show_all_profiles'),
-    path('home/', views.show_all, name='show_all'),  # Show all quotes and images
-    path('about/', views.about, name='about'),  # About page
+    path('/mini_fb/', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
+
 ]
