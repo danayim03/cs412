278483d5e326d92220ac5eba847447f51fe9bb9b
Dana <danayim@bu.edu>
2024-10-07 13:50:36 -0400

    corrected the url path

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('mini_fb/', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
+    path(r'', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
 
 ]
