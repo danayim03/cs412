ff46fba3cfba93a10d653c67cfb05f5b6727b3d0
Dana <danayim@bu.edu>
2024-10-07 13:17:58 -0400

    got rid of url /

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/mini_fb/', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
+    path('mini_fb/', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
 
 ]
