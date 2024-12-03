be600dbde6e5efc1f41f05a3dbeb6089a9640997
Dana <danayim@bu.edu>
2024-12-03 17:38:00 -0500

    Added a search bar for user list.

--- project/views.py
+++ project/views.py
@@ -15,6 +15,16 @@ class UserListView(ListView):
     template_name = 'project/user_list.html'
     context_object_name = 'users'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('user_search', '')
+        if search_query:
+            # Allows searching by author username
+            queryset = queryset.filter(
+                Q(username__icontains=search_query)
+            )
+        return queryset
+
 
 class UserDetailView(DetailView):
     model = User
