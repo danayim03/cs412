71a713d3e36a1ee49eedb829809331c4e8eed8c3
Dana <danayim@bu.edu>
2024-12-03 17:43:38 -0500

    Added a search bar for courses.

--- project/views.py
+++ project/views.py
@@ -38,6 +38,16 @@ class CourseListView(ListView):
     template_name = 'project/course_list.html'
     context_object_name = 'courses'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.GET.get('class_search', '')
+        if search_query:
+            # Allows searching by course name
+            queryset = queryset.filter(
+                Q(course_name__icontains=search_query)
+            )
+        return queryset
+
 
 class CourseDetailView(DetailView):
     model = Course
