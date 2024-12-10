f18e15263915e58a933ee4f7952fca49eb12bcb6
Dana <danayim@bu.edu>
2024-12-09 21:41:14 -0500

    Finished editing admin.py: instead of writing all the models for import, replaced with '*'

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Course, ClassReview, AcademicTrack, AcademicTrackCourse
+from .models import *
 
 admin.site.register(CustomUser)
 admin.site.register(Course)
